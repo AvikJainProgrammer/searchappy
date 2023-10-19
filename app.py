c9ead05a7b7619540f80a94e5b83200a21aad60e
Abhishek Kumar <thisavik@gmail.com>
2023-10-19 16:30:06 +0530

    env setting added

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+project_folder = os.path.dirname(__file__)
 
+load_dotenv(os.path.join(project_folder, '.env'))
+
+env_enable = os.getenv('ENV_ENABLE');
 @app.route("/")
 def index():
-    return "<center><h1>Flask App deployment on AZURE :)</h1></center"
+    return f"<center><h1>Flask App deployment on AZURE {env_enable}</h1></center"
 
 if __name__ == "__main__":
     app.run()
